f4b1352a85841d1d43071e92dbfe19d379190ea7
Mahmoud <mymafm0405@gmail.com>
2021-01-04 19:54:09 +0300

    Too many edit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCx9iiOlxqrzZnBwLkZinCIFAZ9hQ6plFA",
+    authDomain: "absentsapp.firebaseapp.com",
+    databaseURL: "https://absentsapp-default-rtdb.firebaseio.com",
+    projectId: "absentsapp",
+    storageBucket: "absentsapp.appspot.com",
+    messagingSenderId: "928699596631",
+    appId: "1:928699596631:web:99849e2a89dc33c294c812",
+    measurementId: "G-N8LGSX6B3H"
+  }
 };
 
 /*
